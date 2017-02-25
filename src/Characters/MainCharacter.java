011473874cf425f89cc450dad6eb2f48ee5c42bf
Jean-Simon <jeansimoncollard@hotmail.com>
2017-02-25 17:58:05 -0500

    added layer overhead when character goes under roofs

--- src/Characters/MainCharacter.java
+++ src/Characters/MainCharacter.java
@@ -9,9 +9,13 @@ import org.newdawn.slick.tiled.TiledMap;
 public class MainCharacter {
 
 	public int XPosition, YPosition;
-	public Image Image;
-
 	public int _frameCount;
+
+	private final String CHARACTER_IMAGE_UPPERTILE_PATH = "dependencies/characters/maincharacterUpperTile.png";
+	private final String CHARACTER_IMAGE_LOWERTILE_PATH = "dependencies/characters/maincharacterLowerTile.png";
+
+	private Image _upperTileImage;
+	private Image _lowerTileImage;
 
 	public MainCharacter() {
 		XPosition = 0;
@@ -19,10 +23,11 @@ public class MainCharacter {
 		_frameCount = 0;
 	}
 
-	public MainCharacter(int xPosition, int yPosition, String imagePath) throws SlickException {
+	public MainCharacter(int xPosition, int yPosition) throws SlickException {
 		XPosition = xPosition;
 		YPosition = yPosition;
-		Image = new Image(imagePath);
+		_upperTileImage = new Image(CHARACTER_IMAGE_UPPERTILE_PATH);
+		_lowerTileImage = new Image(CHARACTER_IMAGE_LOWERTILE_PATH);
 	}
 
 	public void Move(TiledMap map, boolean isRenderOverlay, GameContainer gc) {
@@ -31,13 +36,13 @@ public class MainCharacter {
 		if (!gc.getInput().isKeyDown(Input.KEY_RIGHT) && !gc.getInput().isKeyDown(Input.KEY_LEFT)
 				&& !gc.getInput().isKeyDown(Input.KEY_UP) && !gc.getInput().isKeyDown(Input.KEY_DOWN)) {
 			_frameCount = 0;// When nothing is pressed, reset the frameCount to
-							// 0, else, move only when _frameCount%30 = 0 (30 can
+							// 0, else, move only when _frameCount%x = 0 (x can
 							// be changed to change movement speed)
 			return;
 		}
 
 		if (!isRenderOverlay) {
-			
+
 			if (gc.getInput().isKeyDown(Input.KEY_RIGHT) && _frameCount % 15 == 0) {
 				// Doesn't go off map
 				if (this.XPosition != map.getWidth() - 1) {
@@ -45,7 +50,7 @@ public class MainCharacter {
 					if (map.getTileId(this.XPosition + 1, this.YPosition, foregroundLayerIndex) != 0) {
 						this.XPosition++;
 					}
-				}	
+				}
 			}
 
 			if (gc.getInput().isKeyDown(Input.KEY_LEFT) && _frameCount % 15 == 0) {
@@ -78,6 +83,27 @@ public class MainCharacter {
 			}
 			_frameCount++;
 		}
+	}
+
+	public void RenderCharacter(TiledMap map, GameContainer gc) {
+		
+		//When the character is on those layers, don't render it
+		int overheadLayerIndex1 = map.getLayerIndex("characterOverhead1"); 
+		int overheadLayerIndex2 = map.getLayerIndex("roofsCharacterOverhead");
+
+		if (map.getTileId(this.XPosition, this.YPosition, overheadLayerIndex1) == 0
+				&& map.getTileId(this.XPosition, this.YPosition, overheadLayerIndex2) == 0) { 
+			this._lowerTileImage.draw(gc.getWidth() / 2 - 4, gc.getHeight() / 2 , 40, 32);
+			this._upperTileImage.draw(gc.getWidth() / 2 - 4, gc.getHeight() / 2 - 32, 40, 32); //Always render top if lower renders
+			return;
+		}
+		
+		if (map.getTileId(this.XPosition, this.YPosition-1, overheadLayerIndex1) == 0
+				&& map.getTileId(this.XPosition, this.YPosition-1, overheadLayerIndex2) == 0) { 
+			this._upperTileImage.draw(gc.getWidth() / 2 - 4, gc.getHeight() / 2 - 32, 40, 32);
+		}
+
+		
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -6,68 +6,63 @@ import Characters.MainCharacter;
 
 public class Main extends BasicGame {
 
-    private TiledMap _map;
-    private MainCharacter _mainCharacter;
-    private Image _img;
-    private boolean _renderOverlay = false;
+	private TiledMap _map;
+	private MainCharacter _mainCharacter;
+	private Image _img;
+	private boolean _renderOverlay = false;
 
-    private final String MAP_PATH = "dependencies/map/bishopsmap.tmx";
-    private final String CHARACTER_IMAGE_PATH = "dependencies/characters/maincharacter.png";
-    private final String OVERLAY_PATH = "dependencies/UI_photos/overlay.png";
+	private final String MAP_PATH = "dependencies/map/bishopsmap.tmx";
+	private final String OVERLAY_PATH = "dependencies/UI_photos/overlay.png";
 
+	public Main(String gamename) {
+		super(gamename);
+	}
 
-    public Main(String gamename) {
-        super(gamename);
-    }
+	@Override
+	// This function is called once at the beginning when we start the game
+	public void init(GameContainer gc) throws SlickException {
+		_map = new TiledMap(MAP_PATH);
+		_mainCharacter = new MainCharacter(135, 85);
+		_img = new Image(OVERLAY_PATH);
+		// Set minimum interval between update() calls
+		gc.setMinimumLogicUpdateInterval(5);
 
-    @Override
-    // This function is called once at the beginning when we start the game
-    public void init(GameContainer gc) throws SlickException {
-        _map = new TiledMap(MAP_PATH);
-        _mainCharacter = new MainCharacter(135, 85, CHARACTER_IMAGE_PATH);
-        _img = new Image(OVERLAY_PATH);
-        //Set minimum interval between update() calls
-        gc.setMinimumLogicUpdateInterval(5);
+	}
 
+	@Override
+	// Update is called just before render
+	public void update(GameContainer gc, int i) throws SlickException {
 
+		_mainCharacter.Move(_map, _renderOverlay, gc);
 
-    }
+		if (gc.getInput().isMousePressed(0) && Mouse.getX() < gc.getWidth() && Mouse.getY() < 136 && Mouse.getY() > 0
+				&& Mouse.getX() > gc.getWidth() - 130) {
+			_renderOverlay = !_renderOverlay;
+		}
+	}
 
-    @Override
-    // Update is called just before render
-    public void update(GameContainer gc, int i) throws SlickException {
-    	_mainCharacter.Move(_map, _renderOverlay, gc);
-    	
-        if (gc.getInput().isMousePressed(0) &&
-                Mouse.getX() < gc.getWidth() &&
-                Mouse.getY() < 136 &&
-                Mouse.getY() > 0 &&
-                Mouse.getX() > gc.getWidth() - 130) {
-            _renderOverlay = !_renderOverlay;
-        }
-    }
+	@Override
+	// This function is called for every FPS
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		// Move the map to simulate that the character moves, but the character
+		// actually stays static in middle of screen {
 
-    @Override
-    // This function is called for every FPS
-    public void render(GameContainer gc, Graphics g) throws SlickException {
-        // Move the map to simulate that the character moves, but the character
-        // actually stays static in middle of screen {
-        _map.render(0, 0, _mainCharacter.XPosition - gc.getWidth() / (2 * 32),
-                _mainCharacter.YPosition - gc.getHeight() / (2 * 32), 60, 60
+		_map.render(0, 0, _mainCharacter.XPosition - gc.getWidth() / (2 * 32),
+				_mainCharacter.YPosition - gc.getHeight() / (2 * 32), 40, 60
 
-        );
+		);
 
-        //Fill a square at middle of screen that represents character
-        _mainCharacter.Image.draw(gc.getWidth() / 2 - 4, gc.getHeight() / 2 - 32, 40, 64);
+		_mainCharacter.RenderCharacter(_map, gc);
 
-        g.drawImage(new Image("dependencies/UI_photos/wood-plank.jpg"), gc.getWidth() - 130, gc.getHeight() - 135);
+		g.drawImage(new Image("dependencies/UI_photos/wood-plank.jpg"), gc.getWidth() - 130, gc.getHeight() - 135);
 
-        if (_renderOverlay) {
-            g.drawImage(_img, (gc.getWidth() / 2) - (_img.getWidth() / 2), (gc.getHeight() / 2) - (_img.getHeight() / 2));
-        }
-    }
+		if (_renderOverlay) {
+			g.drawImage(_img, (gc.getWidth() / 2) - (_img.getWidth() / 2),
+					(gc.getHeight() / 2) - (_img.getHeight() / 2));
+		}
+	}
 
-    public static void main(String[] args) throws SlickException {
+	public static void main(String[] args) throws SlickException {
 
 		AppGameContainer appgc;
 		appgc = new AppGameContainer(new Main("Treasure Trail"));
